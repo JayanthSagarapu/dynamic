5340f7017707ad14540716e0c741c79d52f76506
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-05-25 11:30:56 +0530

    Updated Edit and Delete Buttons actions

--- models/cart.js
+++ models/cart.js
@@ -21,6 +21,7 @@ module.exports = class Cart {
       const existingProduct = cart.products[existingProductIndex];
 
       let updatedProduct;
+
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
@@ -31,8 +32,30 @@ module.exports = class Cart {
         cart.products = [...cart.products, updatedProduct];
       }
 
-      cart.totalPrice = cart.totalPrice + Number(productPrice);
+      cart.totalPrice = cart.totalPrice + +productPrice;
       fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, filecontent) => {
+      if (err) {
+        return;
+      }
+      cart = JSON.parse(filecontent);
+      const presentCart = { ...cart };
+      const prodToDel = presentCart.products.find((prod) => prodid === id);
+      const prodToDelQty = prodToDel.qty;
+
+      presentCart.products = presentCart.products.filter(
+        (prod) => prod.id !== id
+      );
+      presentCart.totalPrice =
+        presentCart.totalPrice - productPrice * prodToDelQty;
+
+      fs.writeFile(p, JSON.stringify(presentCart), (err) => {
         console.log(err);
       });
     });
