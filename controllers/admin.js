b74e5971798a2a8d2e19f599bc936dea075268eb
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-05-26 16:03:08 +0530

    creating table,retrive, fetch,insert,delete data

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect("/");
+  product
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -68,6 +72,9 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.delById(prodId);
-  res.redirect("/admin/products");
+  Product.delById(prodId)
+    .then(() => {
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
